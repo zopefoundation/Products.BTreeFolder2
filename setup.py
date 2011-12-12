8e8042aa95866bfedfabcc12ecb6cc7347ff0e49
Hanno Schlichting <hanno@hannosch.eu>
2011-12-12 12:59:49 +0000

    vb

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.BTreeFolder2',
-      version='2.13.4',
+      version='2.13.5dev',
       url='http://pypi.python.org/pypi/Products.BTreeFolder2',
       license='ZPL 2.1',
       description="A BTree based implementation for Zope 2's OFS.",
