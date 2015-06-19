24487aa6f2a67f985fc061b2de6813c0b504c5f8
Tres Seaver <tseaver@palladion.com>
2015-06-18 21:55:32 -0400

    svb

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.BTreeFolder2',
-      version='2.13.5',
+      version='2.13.6.dev0',
       url='http://pypi.python.org/pypi/Products.BTreeFolder2',
       license='ZPL 2.1',
       description="A BTree based implementation for Zope 2's OFS.",
