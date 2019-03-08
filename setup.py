61ca750cfe2d6746a9b9ebc13e8741c65b370590
Michael Howitz <mh@gocept.com>
2019-03-08 14:03:36 +0100

    Back to development: 4.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.BTreeFolder2',
-      version='4.2',
+      version='4.3.dev0',
       url='https://github.com/zopefoundation/Products.BTreeFolder2',
       license='ZPL 2.1',
       description="A BTree based implementation for Zope 2's OFS.",
