d07cd5924d5d3a767173ebfabd2d9850d0f92fd9
Michael Howitz <mh@gocept.com>
2021-04-26 08:28:34 +0200

    Back to development: 4.4

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,12 @@
 Changelog
 =========
 
+4.4 (unreleased)
+----------------
+
+- Nothing changed yet.
+
+
 4.3 (2021-04-26)
 ----------------
 
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 
 setup(name='Products.BTreeFolder2',
-      version='4.3',
+      version='4.4.dev0',
       url='https://github.com/zopefoundation/Products.BTreeFolder2',
       license='ZPL 2.1',
       description="A BTree based implementation for Zope 2's OFS.",
